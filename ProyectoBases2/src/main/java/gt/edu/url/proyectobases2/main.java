042d9cf30fb7f7fe5ec1433a9ec02b3ec26db615
Fer <ferrafterminador@gmail.com>
2020-09-27 10:29:26 -0600

    conexion a mongoDb

--- ProyectoBases2/src/main/java/gt/edu/url/proyectobases2/main.java
+++ ProyectoBases2/src/main/java/gt/edu/url/proyectobases2/main.java
@@ -5,6 +5,11 @@
  */
 package gt.edu.url.proyectobases2;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 /**
  *
  * @author ferna
@@ -15,7 +20,15 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Proyecto con maven");
+        MongoClientURI uri = new MongoClientURI(
+            "mongodb+srv://fercho:1234fer@cluster0.mld3v.mongodb.net/prueba?retryWrites=true&w=majority");
+        MongoClient mongoClient = new MongoClient(uri);
+        MongoDatabase database = mongoClient.getDatabase("prueba");//nombre de la coleccion
+        //MongoCollection<Document> collection = database.getCollection("prueba");//creo un documento a partir de la coleccion
+        //database.createCollection("usuarios");
+        //System.out.println(database.getName());//obtener el nombre de la base de datos
+        
+        
     }
     
 }
