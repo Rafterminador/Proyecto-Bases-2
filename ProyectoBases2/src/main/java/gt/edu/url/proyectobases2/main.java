b1feced1c8b434c1f78f9598538f791b0bb566d9
Fer <ferrafterminador@gmail.com>
2020-09-27 10:50:19 -0600

    insertando datos en mongodb

--- ProyectoBases2/src/main/java/gt/edu/url/proyectobases2/main.java
+++ ProyectoBases2/src/main/java/gt/edu/url/proyectobases2/main.java
@@ -5,11 +5,14 @@
  */
 package gt.edu.url.proyectobases2;
 
+import com.mongodb.ErrorCategory;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.MongoWriteException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-
+import org.bson.types.ObjectId;
+import org.bson.Document;
 /**
  *
  * @author ferna
@@ -23,11 +26,38 @@ public class main {
         MongoClientURI uri = new MongoClientURI(
             "mongodb+srv://fercho:1234fer@cluster0.mld3v.mongodb.net/prueba?retryWrites=true&w=majority");
         MongoClient mongoClient = new MongoClient(uri);
-        MongoDatabase database = mongoClient.getDatabase("prueba");//nombre de la coleccion
-        //MongoCollection<Document> collection = database.getCollection("prueba");//creo un documento a partir de la coleccion
+        MongoDatabase database = mongoClient.getDatabase("prueba");//nombre de la coleccion o tabla
+        MongoCollection<Document> collection = database.getCollection("cafeteria");//creo un documento a partir de la coleccion
+        System.out.println(collection.countDocuments());//obtiene cantidad de rows de la base de datos
         //database.createCollection("usuarios");
         //System.out.println(database.getName());//obtener el nombre de la base de datos
         
+        
+                    //Insert a document into the "characters" collection.
+            //MongoCollection collection = database.getCollection("characters");
+
+            Document mickeyMouse = new Document();
+            Document charlieBrown = new Document();
+
+            mickeyMouse.append("_id", 1)
+                    .append("characterName", "Mickey Mouse")
+                    .append("creator", new Document("firstName", "Walt").append("lastName", "Disney"))
+                    .append("pet", "Goofy");
+
+            charlieBrown.append("_id", 2)
+                    .append("characterName", "Charlie Brown")
+                    .append("creator", new Document("firstName", "Charles").append("lastName", "Shultz"))
+                    .append("pet", "Snoopy");
+
+            try {
+                collection.insertOne(mickeyMouse);
+                collection.insertOne(charlieBrown);
+                System.out.println("Successfully inserted documents. \n");
+            } catch (MongoWriteException mwe) {
+                if (mwe.getError().getCategory().equals(ErrorCategory.DUPLICATE_KEY)) {
+                    System.out.println("Document with that id already exists");
+                }
+            }
         
     }
     
